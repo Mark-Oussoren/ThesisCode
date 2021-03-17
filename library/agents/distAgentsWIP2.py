1e5d64f7b64f9ef34f479794bb76d87f18c57811
Mark Oussoren <49008466+Mark-Oussoren@users.noreply.github.com>
2021-03-16 22:31:09 -0400

    tab-spacing and redundant if-else clauses

--- library/agents/distAgentsWIP2.py
+++ library/agents/distAgentsWIP2.py
@@ -365,28 +365,25 @@ class QRAgent(distAgent):
 		return loss
 		
 
-	def _build_model(self,target = False):
+	def _build_model(self, target=False):
 		# Using Keras functional API
-		state_in = Input(shape=(self.state_size + self.action_space_size,))
+		state_in = Input(shape=(self.state_size + self.action_space_size, ))
 		# If using market data
 		if self.n_hist_data > 0:
-			state_in = Input(shape=(self.state_size + self.action_space_size,))
-			if self.multiply_layers:
-				state_in_r = Reshape((self.state_size + self.action_space_size,1,))(state_in)
+			state_in = Input(shape=(self.state_size + self.action_space_size, ))
 			if target:
 				hist_model = self.hist_target_model
 			else:
 				hist_model = self.hist_model
-	
- 
- 			if self.multiply_layers:
-				hist_out = hist_model.output
-				input_layer = Dot(axes=(2, 2))([state_in_r,hist_out])
-				input_layer = Flatten()(input_layer)
+			if self.multiply_layers:
+					state_in_r = Reshape((self.state_size + self.action_space_size, 1, ))(state_in)
+					hist_out = hist_model.output
+					input_layer = Dot(axes=(2, 2))([state_in_r, hist_out])
+					input_layer = Flatten()(input_layer)
 			else:
-				input_layer = concatenate([state_in,hist_model.output])
+					input_layer = concatenate([state_in, hist_model.output])
 		else:
-			input_layer = state_in
+				input_layer = state_in
 
 		# Testing adding skip layers and dropout
 		skip_layer = Dense(self.model_units, activation='relu')(input_layer)
